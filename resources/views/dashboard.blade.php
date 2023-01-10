9b150229424367ab91989a0d5bd62a8a8387f228
Leonardo Vellozo <leonardo@verticis.com.br>
2023-01-09 23:29:52 -0300

    [UPDATE] => importing template and created model and controler at dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
     
-    <div class="py-12">
+    {{-- <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
@@ -13,5 +13,201 @@
                 </div>
             </div>
         </div>
+    </div> --}}
+    <div class="dashboard-wrapper">
+        <div class="dashboard-finance">
+            <div class="container-fluid dashboard-content">
+                <!-- ============================================================== -->
+                <!-- pageheader  -->
+                <!-- ============================================================== -->
+                {{-- <div class="row">
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                        <div class="page-header">
+                            <h3 class="mb-2">Finance Dashboard </h3>
+                            <p class="pageheader-text">Proin placerat ante duiullam scelerisque a velit ac porta, fusce sit amet vestibulum mi. Morbi lobortis pulvinar quam.</p>
+                            <div class="page-breadcrumb">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page">Finance Dashboard Template</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div> --}}
+                <!-- ============================================================== -->
+                <!-- end pageheader  -->
+                <!-- ============================================================== -->
+                <div class="row">
+                    <div class="offset-xl-10 col-xl-2 col-lg-2 col-md-6 col-sm-12 col-12">
+                        <form>
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="daterange" value="01/01/2018 - 01/15/2018" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <h5 class="card-header">Total Income</h5>
+                            <div class="card-body">
+                                <div class="metric-value d-inline-block">
+                                    <h1 class="mb-1">$5,79,000</h1>
+                                </div>
+                                <div class="metric-label d-inline-block float-right text-success font-weight-bold">
+                                    <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">25%</span>
+                                </div>
+                            </div>
+                            <div class="card-body bg-light p-t-40 p-b-40">
+                                <div id="sparkline-revenue"></div>
+                            </div>
+                            <div class="card-footer text-center bg-white">
+                                <a href="#" class="card-link">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <h5 class="card-header">Total Expences</h5>
+                            <div class="card-body">
+                                <div class="metric-value d-inline-block">
+                                    <h1 class="mb-1">$1,79,000</h1>
+                                </div>
+                                <div class="metric-label d-inline-block float-right text-secondary font-weight-bold">
+                                    <span class="icon-circle-small icon-box-xs text-danger bg-danger-light"><i class="fa fa-fw fa-arrow-down"></i></span><span class="ml-1">15%</span>
+                                </div>
+                            </div>
+                            <div class="card-body text-center bg-light p-t-40 p-b-40">
+                                <div id="sparkline-revenue2"></div>
+                            </div>
+                            <div class="card-footer text-center bg-white">
+                                <a href="#" class="card-link">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <h5 class="card-header">Cash on Hand</h5>
+                            <div class="card-body">
+                                <div class="metric-value d-inline-block">
+                                    <h1 class="mb-1">$79,000</h1>
+                                </div>
+                                <div class="metric-label d-inline-block float-right text-success font-weight-bold">
+                                    <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span><span class="ml-1">25%</span>
+                                </div>
+                            </div>
+                            <div class="card-body bg-light p-t-40 p-b-40">
+                                <div id="sparkline-revenue3"></div>
+                            </div>
+                            <div class="card-footer text-center bg-white">
+                                <a href="#" class="card-link">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="card">
+                            <h5 class="card-header">Net Profit Margin</h5>
+                            <div class="card-body">
+                                <div class="metric-value d-inline-block">
+                                    <h1 class="mb-1">$92,000</h1>
+                                </div>
+                                <div class="metric-label d-inline-block float-right text-success font-weight-bold">
+                                    <span class="icon-circle-small icon-box-xs text-success bg-success-light"><i class="fa fa-fw fa-arrow-up"></i></span> <span class="ml-1">25%</span>
+                                </div>
+                            </div>
+                            <div class="card-body bg-light p-b-40 p-t-40">
+                                <div id="sparkline-revenue4"></div>
+                            </div>
+                            <div class="card-footer text-center bg-white">
+                                <a href="#" class="card-link">View Details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- end revenue year  -->
+                <!-- ============================================================== -->
+                
+            </div>            
+            <div class="row">
+                <!-- ============================================================== -->
+                <!-- basic table  -->
+                <!-- ============================================================== -->
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                    <div class="card">
+                        <h5 class="card-header">Last Releases</h5>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered first">
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Position</th>
+                                            <th>Office</th>
+                                            <th>Age</th>
+                                            <th>Start date</th>
+                                            <th>Salary</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Tiger Nixon</td>
+                                            <td>System Architect</td>
+                                            <td>Edinburgh</td>
+                                            <td>61</td>
+                                            <td>2011/04/25</td>
+                                            <td>$320,800</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Garrett Winters</td>
+                                            <td>Accountant</td>
+                                            <td>Tokyo</td>
+                                            <td>63</td>
+                                            <td>2011/07/25</td>
+                                            <td>$170,750</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Ashton Cox</td>
+                                            <td>Junior Technical Author</td>
+                                            <td>San Francisco</td>
+                                            <td>66</td>
+                                            <td>2009/01/12</td>
+                                            <td>$86,000</td>
+                                        </tr>
+                                    </tbody>                                   
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ============================================================== -->
+                <!-- end basic table  -->
+                <!-- ============================================================== -->
+            </div>                 
+        </div>
+        <!-- ============================================================== -->
+        <!-- footer -->
+        <!-- ============================================================== -->
+        <div class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        Copyright © 2018 Concept. All rights reserved. Dashboard by <a href="https://colorlib.com/wp/">Colorlib</a>. 
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
+                        <div class="text-md-right footer-links d-none d-sm-block">
+                            <a href="javascript: void(0);">About</a>
+                            <a href="javascript: void(0);">Support</a>
+                            <a href="javascript: void(0);">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- end footer -->
+        <!-- ============================================================== -->
     </div>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,31 +8,82 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-       
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="{{asset('build/assets/css/style.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/bootstrap/css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/fonts/circular-std/style.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/fonts/fontawesome/css/fontawesome-all.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/vector-map/jqvmap.css')}}">
+        <link rel="stylesheet" href="{{asset('buid/assets/vendor/jvectormap/jquery-jvectormap-2.0.2.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/charts/chartist-bundle/chartist.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/charts/c3charts/c3.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/charts/morris-bundle/morris.css')}}">
+        <link rel="stylesheet" href="{{asset('build/assets/vendor/daterangepicker/daterangepicker.css')}}">
+        
+        {{-- <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
+
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js',])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js',]) --}}
     </head>
     <body class="font-sans antialiased">
+        @include('layouts.menu')
+        @include('layouts.leftMenu')
         <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-            {{-- @include('layouts.menu')
-            @include('layouts.leftMenu') --}}
+            {{-- @include('layouts.navigation') --}}
 
             <!-- Page Heading -->
-            @if (isset($header))
+            {{-- @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endif --}}
 
             <!-- Page Content -->
-            <main>
+             <main>
                 {{ $slot }}
-            </main>
+            </main> 
         </div>
+        <!-- jquery 3.3.1  -->
+    <script src="{{asset('build/assets/vendor/jquery/jquery-3.3.1.min.js')}}"></script>
+    <!-- bootstap bundle js -->
+    <script src="{{asset('build/assets/vendor/bootstrap/js/bootstrap.bundle.js')}}"></script>
+    <!-- slimscroll js -->
+    <script src="{{asset('build/assets/vendor/slimscroll/jquery.slimscroll.js')}}"></script>
+    <!-- chart chartist js -->
+    <script src="{{asset('build/assets/vendor/charts/chartist-bundle/chartist.min.js')}}"></script>
+    <script src="{{asset('build/assets/vendor/charts/chartist-bundle/Chartistjs.js')}}"></script>
+    <script src="{{asset('build/assets/vendor/charts/chartist-bundle/chartist-plugin-threshold.js')}}"></script>
+    <!-- chart C3 js -->
+    <script src="{{asset('build/assets/vendor/charts/c3charts/c3.min.js')}}"></script>
+    <script src="{{asset('build/assets/vendor/charts/c3charts/d3-5.4.0.min.js')}}"></script>
+    <!-- chartjs js -->
+    <script src="{{asset('build/assets/vendor/charts/charts-bundle/Chart.bundle.js')}}"></script>
+    <script src="{{asset('build/assets/vendor/charts/charts-bundle/chartjs.js')}}"></script>
+    <!-- sparkline js -->
+    <script src="{{asset('build/assets/vendor/charts/sparkline/jquery.sparkline.js')}}"></script>
+    <!-- dashboard finance js -->
+    <script src="{{asset('build/assets/js/dashboard-finance.js')}}"></script>
+    <!-- main js -->
+    <script src="{{asset('build/assets/js/main-js.js')}}"></script>
+    <!-- gauge js -->
+    <script src="{{asset('buid/assets/vendor/gauge/gauge.min.js')}}"></script>
+    <!-- morris js -->
+    <script src="{{asset('buid/assets/vendor/charts/morris-bundle/raphael.min.js')}}"></script>
+    <script src="{{asset('buid/assets/vendor/charts/morris-bundle/morris.js')}}"></script>
+    <script src="{{asset('buid/assets/vendor/charts/morris-bundle/morrisjs.html')}}"></script>
+    <!-- daterangepicker js -->
+    <script src="../../../../cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script src="../../../../cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script>
+    $(function() {
+        $('input[name="daterange"]').daterangepicker({
+            opens: 'left'
+        }, function(start, end, label) {
+            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+        });
+    });
+    </script>       
     </body>
 </html>
